b17cc84b5f168dd9bf9ef476385f249de711cb32
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2022-03-28 21:04:07 +0200

    Adjusted API

---
+++ book_controller.py
@@ -0,0 +1,40 @@
+from db import get_db
+
+def insert_book(name, price, email):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "INSERT INTO books(name, price, email) VALUES (?, ?, ?)"
+    cursor.execute(statement, [name, price, email])
+    db.commit()
+    return True
+
+def update_book(id, name, price, email):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "UPDATE books SET name = ?, price = ?, email = ? WHERE id = ?"
+    cursor.execute(statement, [name, price, email, id])
+    db.commit()
+    return True
+
+def delete_book(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "DELETE FROM books WHERE id = ?"
+    cursor.execute(statement, [id])
+    db.commit()
+    return True
+
+def get_by_id(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "SELECT * FROM books WHERE id = ?"
+    cursor.execute(statement, [id])
+    return cursor.fetchone()
+
+
+def get_books():
+    db = get_db()
+    cursor = db.cursor()
+    query = "SELECT * FROM books"
+    cursor.execute(query)
+    return cursor.fetchall()
