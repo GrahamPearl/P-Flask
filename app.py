a8271e34f7f710598a8508e4e3df73de15e2a05f
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2022-03-28 21:04:36 +0200

    Adjusted api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
+import book_controller
 from markupsafe import escape
-from flask import Flask, abort, render_template
+from flask import Flask, abort, render_template, jsonify, request
 app = Flask(__name__)
 
 
@@ -9,12 +10,29 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
-@app.route('/login/<int:user_id>/')
+@app.route('/login/')
 def greet_user():    
     try:
-        return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+        return render_template('login.html', utc_dt=datetime.datetime.utcnow())
     except IndexError:
         abort(404)
+
+@app.route('/books', methods=["GET"])
+def get_books():
+    books = book_controller.get_books()
+    return jsonify(books)
+
+@app.route("/book", methods=["POST"])
+def insert_game():
+    book_details = request.get_json()    
+    result = book_controller.insert_game(book_details["name"], book_details["price"], book_details["email"])
+    return jsonify(result)
+
+@app.route('/book/', methods=["PUT"])
+def update_book():
+    book_details = request.get_json()    
+    result = book_controller.update_list(book_details["id"], book_details["name"], book_details["price"], book_details["email"])
+    return jsonify(result)
 
 @app.route('/about/')
 def about():
