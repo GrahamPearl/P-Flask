e79073620de27288d1e92908610df5c0fcb5d4ce
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2022-03-28 19:56:44 +0200

    Template Login

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+import datetime
+from markupsafe import escape
+from flask import Flask, abort, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+@app.route('/users/<int:user_id>/')
+def greet_user(user_id):
+    users = ['Bob', 'Jane', 'Adam']
+    try:
+        return '<h2>Hi {}</h2>'.format(users[user_id])
+    except IndexError:
+        abort(404)
+
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
 
 if __name__ == '__main__':
     app.run()
